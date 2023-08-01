cd278f6e1f6f1768936e68fb696bad44aa03ade2
Spaceworm12 <127404549+Spaceworm12@users.noreply.github.com>
2023-08-01 13:29:01 +0300

    FC

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("kotlin_kapt")
+    id("kotlin-parcelize")
 }
 
 android {
@@ -9,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.rickandmorty"
-        minSdk = 25
+        minSdk = 30
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
@@ -39,31 +41,67 @@ android {
     buildFeatures {
         compose = true
     }
+    buildFeatures {
+        viewBinding = true
+    }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
     }
 }
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.0")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation ("androidx.room:room-common:2.5.2")
+
+
+    //room db
+    kapt ("androidx.room:room-compiler:2.5.1")
+    implementation ("androidx.room:room-ktx:2.5.1")
+    implementation ("androidx.room:room-runtime:2.5.1")
+    implementation ("androidx.room:room-rxjava2:2.5.1")
+
+    //rx
+    implementation ("io.reactivex.rxjava2:rxandroid:2.1.1")
+    implementation ("io.reactivex.rxjava2:rxjava:2.2.21")
+    implementation ("io.reactivex.rxjava2:rxkotlin:2.4.0")
+
+    // Retrofit - сетевые запросы
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:okhttp:4.9.1")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.9.1")
+    implementation ("com.squareup.retrofit2:adapter-rxjava2:2.9.0'")
+
+    //coil - загрузка изображений
+    implementation ("io.coil-kt:coil:1.4.0")
+    implementation ("io.coil-kt:coil-compose:1.4.0")
+
+
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+    implementation ("androidx.core:core-ktx:1.7.0")
+    implementation ("androidx.appcompat:appcompat:1.6.1")
+    implementation ("com.google.android.material:material:1.8.0")
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation ("androidx.navigation:navigation-fragment-ktx:2.4.1")
+    implementation ("androidx.navigation:navigation-ui-ktx:2.4.1")
+    testImplementation ("junit:junit:4.13.2")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.2.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+
+    implementation ("androidx.compose:compose-bom:2022.10.00")
+    implementation ("androidx.compose.ui:ui-graphics")
+    implementation ("androidx.compose.material3:material3")
+    androidTestImplementation ("androidx.compose:compose-bom:2022.10.00")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4")
+    debugImplementation ("androidx.compose.ui:ui-tooling")
+    debugImplementation ("androidx.compose.ui:ui-test-manifest")
+    api("androidx.compose.runtime:runtime-livedata:1.4.0")
+    api("androidx.compose.ui:ui-tooling:1.4.0")
+    api("androidx.compose.ui:ui-tooling-preview:1.4.0")
+    api("androidx.compose.ui:ui:1.4.0")
+    api("androidx.compose.material:material:1.4.0")
+    api("androidx.compose.runtime:runtime-livedata:1.4.0")
+    implementation("androidx.compose.material:material-icons-extended:1.4.0")
 }
