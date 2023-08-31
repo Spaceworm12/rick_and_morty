5e35087c7951dea897140c967f06d86e725cba08
Viktor Radchenko <spaceworm1@yandex.ru>
2023-08-31 12:47:28 +0300

    lalala

---
+++ app/src/main/java/com/example/rickandmorty/presentation/navigation/Screens.kt
@@ -0,0 +1,42 @@
+package com.example.rickandmorty.presentation.navigation
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentFactory
+import com.example.rickandmorty.presentation.category.CategoryListFragment
+import com.example.rickandmorty.presentation.detailperson.DetailPersonFragment
+import com.example.rickandmorty.presentation.favorites.FavoritesListFragment
+import com.example.rickandmorty.presentation.favoritesdetail.FavoritesDetailPersonFragment
+import com.example.rickandmorty.presentation.listperson.PersonListFragment
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+
+class Screens {
+    class CategoryScreen : FragmentScreen {
+        override fun createFragment(factory: FragmentFactory): Fragment {
+            return CategoryListFragment()
+        }
+    }
+
+    class ListPersonsScreen : FragmentScreen {
+        override fun createFragment(factory: FragmentFactory): Fragment {
+            return PersonListFragment()
+        }
+    }
+
+    class ListFavoritePersonsScreen : FragmentScreen {
+        override fun createFragment(factory: FragmentFactory): Fragment {
+            return FavoritesListFragment()
+        }
+    }
+
+    class PersonScreen : FragmentScreen {
+        override fun createFragment(factory: FragmentFactory): Fragment {
+            return DetailPersonFragment()
+        }
+    }
+
+    class FavoritePersonScreen : FragmentScreen {
+        override fun createFragment(factory: FragmentFactory): Fragment {
+            return FavoritesDetailPersonFragment()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/rickandmorty/presentation/navigation/Wrs2Coordinator.kt
@@ -0,0 +1,91 @@
+package com.example.rickandmorty.presentation.navigation
+
+import com.github.terrakok.cicerone.Router
+import com.github.terrakok.cicerone.Screen
+import com.github.terrakok.cicerone.androidx.FragmentScreen
+import ru.x5.core.navigation.Coordinator
+import ru.x5.core.navigation.SharedScreen
+import ru.x5.utils.matomo.Matomo
+import java.util.*
+
+@Suppress("NAME_SHADOWING")
+class Wrs2Coordinator(
+    private val router: Router,
+    private val mapper: SharedScreenMapper
+) : Coordinator {
+
+    private val stack = ArrayDeque<Screen>()
+
+    override fun goTo(screen: Screen) {
+        val screen = map(screen)
+        router.navigateTo(screen)
+        stack.push(screen)
+        trackScreen()
+    }
+
+    override fun goToDistinct(screen: Screen) {
+        val screen = map(screen)
+        if (stack.isNotEmpty() && stack.peek()!!.screenKey == screen.screenKey) {
+            router.exit()
+            stack.pop()
+        }
+        goTo(screen)
+    }
+
+    override fun goBack() {
+        router.exit()
+        if (stack.isNotEmpty()) stack.pop()
+        trackScreen()
+    }
+
+    override fun goBackTo(screen: Screen) {
+        val screen = map(screen)
+        router.backTo(screen)
+        while (stack.isNotEmpty() && stack.peek()!!.screenKey != screen.screenKey) stack.pop()
+        trackScreen()
+    }
+
+    override fun replace(screen: Screen) {
+        val screen = map(screen)
+        router.replaceScreen(screen)
+        if (stack.isNotEmpty()) stack.pop()
+        stack.push(screen)
+        trackScreen()
+    }
+
+    override fun newRootScreen(screen: Screen) {
+        val screen = map(screen)
+        router.newRootScreen(screen)
+        stack.clear()
+        stack.push(screen)
+        trackScreen()
+    }
+
+    override fun clearStack() {
+        stack.clear()
+    }
+
+    private fun map(screen: Screen): Screen {
+        return if (screen is SharedScreen) mapper(screen)
+        else screen
+    }
+
+    private fun trackScreen() {
+        if (stack.isEmpty()) return
+        val screen = getScreenName(stack.peekFirst()!!)
+        val title = stack.reversed().joinToString(
+            separator = "/",
+            prefix = "/",
+            transform = { getScreenName(it) }
+        )
+        Matomo.trackScreen(screen, title)
+    }
+
+    private fun getScreenName(screen: Screen): String {
+        return if (screen is FragmentScreen) {
+            screen.javaClass?.simpleName ?: "null"
+        } else {
+            screen.javaClass.simpleName
+        }
+    }
+}
